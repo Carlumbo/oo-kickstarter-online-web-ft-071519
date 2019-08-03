302199a3dc875d8df8e4cb76212d337e51b0ec44
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-08-03 19:13:54 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,14 +1,16 @@
 class Backer
-
-  attr_accessor :name, :backed_projects
-
+  attr_accessor :backed_projects
+  attr_reader :name
+  
   def initialize(name)
     @name = name
     @backed_projects = []
-  end
-
-  def back_project(project)
+  end 
+  
+  def back_projects(project)
     @backed_projects << project
     project.backers << self
-  end
-end
+  end 
+  
+  
+end 
