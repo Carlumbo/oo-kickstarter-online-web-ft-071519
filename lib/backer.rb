6c667e83330f0ebbd40f6c58bcedf709492d6a8a
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-08-03 19:10:11 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -4,11 +4,11 @@ class Backer
   
   def initialize(name)
     @name = name
-    @@backed_projects = []
+    @backed_projects = []
   end 
   
   def back_projects(project)
-    @@backed_projects << project
+    @backed_projects << project
   end 
   
   
